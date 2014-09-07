39f8397d86479d84c3fe41684e9930bc84b96732
PirataNervo <piratanervo@gmail.com>
2014-09-07 13:07:02 +0100

    Preparing 2.0

--- Upload/inc/plugins/newpoints.php
+++ Upload/inc/plugins/newpoints.php
@@ -62,7 +62,7 @@ elseif(THIS_SCRIPT == 'member.php')
     $templatelist .= 'newpoints_profile,newpoints_donate_inline';
 }
 	
-define('NEWPOINTS_VERSION', '1.9.8');
+define('NEWPOINTS_VERSION', '2.0');
 
 // load plugins and do other stuff
 if (defined('IN_ADMINCP'))
